9d57630b719565cc99996daaa6b7e7aebce3afba
rytikovroman1994 <bboycooler@yandex.ru>
2020-10-17 12:31:05 +0300

    fix

--- packages/front/pages/index.tsx
+++ packages/front/pages/index.tsx
@@ -1,18 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic'
 
 const MediumEditor = dynamic(() => import('components/Editor'),{ ssr: false })
 
-import { Flex, Heading, Spinner, Button } from 'bumbag';
-
-import Link from 'next/link';
-import { useGetProjectsQuery } from 'graphqlTypes';
+import { Flex } from 'bumbag';
 
 export default function CourseList() {
-
-useEffect(()=>{
-  
-},[])
   return (
 		<Flex padding="major-2" flexBasis="30%" flexDirection="column">
 			<Flex padding="major-2" flexBasis="30%" flexDirection="column">
